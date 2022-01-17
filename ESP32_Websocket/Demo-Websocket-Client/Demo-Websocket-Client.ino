7c862897fd970ff8a463a122a69e4b2f241fdcc8
rico <riccardo_zambelli@hotmail.de>
2022-01-18 00:22:59 +0100

    added simple buzzer init

--- ESP32_Websocket/Demo-Websocket-Client/Demo-Websocket-Client.ino
+++ ESP32_Websocket/Demo-Websocket-Client/Demo-Websocket-Client.ino
@@ -3,40 +3,74 @@
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
 
+//Define pins
+#define BUZZERPIN 12
+#define LEDPIN 13
 
 // Initialize network parameters
-const char* SSID = "SSID"; 
+const char* SSID = "SSID";
 const char* PASSWORD = "PASSWORD";
 
 
 // Initialize server parameters
-const char* SERVER_IP = "SERVER_IP";
+const char* SERVER_IP = "192.168.178.37";
 const int SERVER_PORT = 81; //Replace with server port
 const char* PATH = "/"; //Replace with URL path
 
-// Declare websocket client class variable
+// Web socket
 WebSocketsClient webSocket;
+uint8_t socketReady = 0;
 
 // Allocate the JSON document
 StaticJsonDocument<200> outputData;
 StaticJsonDocument<200> inputData;
 
-uint16_t startMillis;
-uint16_t currentMillis;
+//Configure waiting period
+uint16_t startMillis = 0;
+uint16_t currentMillis = 0;
 const uint16_t period = 2000;
 
+
+//Buzzer 
+struct buzzer {
+    uint8_t addr = 0;
+    char timestamp[50];
+    char team[10];
+    uint8_t buzzerPushed = 0;
+    uint8_t initialized = 0;
+};
+
+buzzer buzzer;
+/*uint8_t addr = 0;
+uint8_t buzzerPushed = 0;
+uint8_t initialized = 0;*/
+
+void IRAM_ATTR ISR(){
+    buzzer.buzzerPushed = 1;
+}
+
 void setup() {
-  WiFi.begin(SSID,PASSWORD);
   Serial.begin(115200);
-  Serial.print("Connecting");
+
+  //interrupt
+  pinMode(BUZZERPIN, INPUT);
+  attachInterrupt(BUZZERPIN, ISR, HIGH);
+  
+  //LED
+  pinMode(LEDPIN, OUTPUT);
+  
+  //Connecting to WiFi
   uint8_t timeoutCounter = 0;
+  WiFi.begin(SSID,PASSWORD);
+  Serial.print("Connecting");
   while(WiFi.status() != WL_CONNECTED && timeoutCounter < 15) {
     Serial.print(".");
     delay(1000);
     timeoutCounter++;
   }
   Serial.println();
-  Serial.print("IP Address: "); Serial.println(WiFi.localIP());
+  Serial.print("IP Address: "); 
+  Serial.println(WiFi.localIP());
 
   // server address, port, and URL path
   webSocket.begin(SERVER_IP, SERVER_PORT, PATH);
@@ -46,55 +80,55 @@ void setup() {
 
   // try ever 5000 again if connection has failed
   webSocket.setReconnectInterval(5000);
-
-  
-  startMillis = millis();
-  Serial.print(startMillis);
   Serial.print("Waiting for socket");
   
 }
 
 void loop() {
-  
-  while(currentMillis - startMillis <= period){
-    webSocket.loop();
-    
-    currentMillis = millis();
-    Serial.print(".");
-    Serial.print(currentMillis - startMillis);
-    
+  delay(1);
+
+  if(socketReady != 0 && buzzer.initialized == 0){
+        Serial.println("Go to init()");
+        initialize();
+   }
+
+  if(buzzer.buzzerPushed != 0){
+      sendPing();
+      buzzer.buzzerPushed = 0;
   }
-  /*currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
-  if (currentMillis - startMillis >= period && currentMillis - startMillis <= period*1.1)  //test whether the period has elapsed
-  {*/
-    sendPing();
-    webSocket.loop();
-    
-  /*}
-  else if(currentMillis - 2 * startMillis >= period){
-    Serial.println(".");
-  }
-  else{
-    Serial.print(".");
-  }
-  */
+  webSocket.loop();
 }
     
 void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
    // get information from server
     String payloadString = (const char*)payload;
     Serial.print("payloadString= ");
-    Serial.println(payloadString);
+    Serial.print(payloadString);
+    Serial.print(" payloadType= ");
+    Serial.println(type);
 
     const char* str;
     if (type == WStype_TEXT) {
+        Serial.println("Deserialze...");
         deserializeJson(inputData, (char*)payload);
-        str = inputData["message"];
+        //Serial.println((const unsigned char*)inputData["adress"]);
+        //char str[10];
+        //strncpy(str, (const char*) inputData["adress"], 10);
+        buzzer.addr = atoi((const char*)inputData["address"]);
+        //Serial.println(str);        
+        //buzzer.addr = atoi(str);
+        Serial.print("Buzzer addr: ");
+        Serial.println(buzzer.addr);
+        /*if (buzzer.addr != 0) {
+            buzzer.initialized = 1;
+            Serial.println("Initialized.");
+        }*/
         Serial.print("Received: ");
-        Serial.println(str);
-        if (strcmp(str, "Hello Client") == 0) {
-            sendPing();
-        }
+        Serial.println(buzzer.addr);        
+    }
+    else if(type == WStype_CONNECTED){
+        Serial.println("Socket is ready");
+        socketReady = true;
     }
 }
 
@@ -106,5 +140,25 @@ void sendPing() {
     webSocket.sendTXT(JSONstr);
     Serial.print("Sent: ");
     Serial.println((const char*)outputData["message"]);
-    delay(1000);
+    
+}
+
+void initialize(){
+    StaticJsonDocument<200> init;
+    
+    char str[10];
+    itoa(buzzer.addr,str,10);
+    Serial.println(str);
+    init["address"] = str;
+    Serial.println((const char*)init["adress"]);
+    //init["timestamp"] = millis();
+    init["team"] = "1";
+    
+    char JSONstr[50];
+    serializeJson(init, JSONstr);
+    Serial.print("Sent: ");
+    Serial.println(JSONstr);
+    webSocket.sendTXT(JSONstr);
+    Serial.println("Initializing...");
+    buzzer.initialized = 1;
 }
--- ESP32_Websocket/Demo-Websocket-Server/Demo-Websocket-Server.ino
+++ ESP32_Websocket/Demo-Websocket-Server/Demo-Websocket-Server.ino
@@ -1,23 +1,31 @@
-//==================================
-//ESP32 WebSocket Server: Toggle LED
-//by: Ulas Dikme
-//==================================
+
 #include <WiFi.h>
 #include <WebServer.h>
 #include <WebSocketsServer.h>
 #include <ArduinoJson.h>
-//-----------------------------------------------
+#include <vector>
+
 const char* SSID = "SSID"; 
 const char* PASSWORD = "PASSWORD";
-//-----------------------------------------------
+
 #define LED 2
-//-----------------------------------------------
+
 WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
-//-----------------------------------------------
+
 StaticJsonDocument<200> outputData;
 StaticJsonDocument<200> inputData;
 
+//struct buzzer
+struct buzzer {
+    uint8_t addr = 0;
+    char timestamp[50];
+    char team[10];
+    uint8_t buzzerPushed = 0;
+    uint8_t initialized = 0;
+};
+
+std::vector<buzzer> buzzerList;
 
 void setup()
 {
@@ -60,39 +68,35 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t welengt
 {
    
 
-
-  String payloadString = (const char *)payload;
-  Serial.print("payloadString= ");
-  Serial.println(payloadString);
+    if (payload != nullptr) {
+      Serial.print("payloadString= ");
+      String payloadString = (const char *)payload;
+      Serial.println(payloadString);
+    }
   
-  const char* str;
+  const char str[] = "test";
   if(type == WStype_TEXT){
     deserializeJson(inputData,(char *)payload);
-    str = inputData["message"];
-    Serial.print("Received: ");
-    Serial.println(str);
+    if (strcmp(inputData["address"], "0") == 0) {
+        buzzer buzzer;
+        buzzer.addr = buzzerList.size() + 1;
+        Serial.print("buzzer addr: ");
+        Serial.println(buzzer.addr);
+        char str[10];
+        itoa(buzzerList.size() + 1, str, 10);
+        strncpy((char*)buzzer.team, str, 10);
+        Serial.print("buzzer team: ");
+        Serial.println(buzzer.team);
+        buzzer.initialized = 1;
+        //buzzer.team = str; 
+        buzzerList.push_back(buzzer);
+        configureBuzzer(&buzzerList.back());
+    }
     if(strcmp(str,"Hello Server")==0){
         sendPong();
     }
   }
 
-  /*if(type == WStype_TEXT) //receive text from client
-  {
-    byte separator=payloadString.indexOf('=');
-    String var = payloadString.substring(0,separator);
-    Serial.print("var= ");
-    Serial.println(var);
-    String val = payloadString.substring(separator+1);
-    Serial.print("val= ");
-    Serial.println(val);
-    Serial.println(" ");
-
-    if(var == "LEDonoff")
-    {
-      LEDonoff = false;
-      if(val == "ON") LEDonoff = true;
-    }
-  }*/
 }
 
 void sendPong(){
@@ -103,4 +107,27 @@ void sendPong(){
     Serial.print("Sent: ");
     Serial.println((const char*)outputData["message"]);
     
+}
+
+void configureBuzzer(buzzer* buzzer) {
+    StaticJsonDocument<200> config;
+    char str[10];
+    itoa(buzzer->addr, str, 10);
+    Serial.print("config buzzer addr: ");
+    Serial.println(str);
+    config["address"] = str;
+    for (int i = 0; i < strlen(str); i++) {
+        str[i] = '\0';
+    }
+    config["team"] = buzzer->team;
+    itoa(buzzer->initialized, str, 10);
+    Serial.print("config buzzer init: ");
+    Serial.println(str);
+    config["init"] = str;
+    char JSONstr[50];
+    serializeJson(config, JSONstr);
+    Serial.print("config buzzer JSONstr: ");
+    Serial.println(JSONstr);
+    webSocket.broadcastTXT(JSONstr);
+    Serial.println("leaving configureBuzzer");
 }
